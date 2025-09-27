80582c9197cdd218689b1ad1cda96e6e447f7ab2
Sam McGarry <smcgwithmc@gmail.com>
2025-09-27 08:34:39 -0700

    went to subway

---
+++ sandwich.py
@@ -0,0 +1,15 @@
+class Sandwich():
+  _bread: object
+  _sauce: object
+  _cheese: object
+  _tomato: object
+  _lettuce: object
+  _meat: object
+
+  __init__():
+    _bread = None
+    _sauce = None
+    _cheese = None
+    _tomato = None
+    _lettuce = None
+    _meat = None

0df4b06333e11fe91704184a9e2a5bf2ada6e162
Sam McGarry <smcgwithmc@gmail.com>
2025-09-27 08:46:05 -0700

    Added mayo

--- sandwich.py
+++ sandwich.py
@@ -1,15 +1,15 @@
-class Sandwich():
-  _bread: object
-  _sauce: object
-  _cheese: object
-  _tomato: object
-  _lettuce: object
-  _meat: object
+class Sandwich:
+    _bread: object
+    _sauce: object
+    _cheese: object
+    _tomato: object
+    _lettuce: object
+    _meat: object
 
-  __init__():
-    _bread = None
-    _sauce = None
-    _cheese = None
-    _tomato = None
-    _lettuce = None
-    _meat = None
+    def __init__(self):
+        self._bread = None
+        self._sauce = "mayonnaise"
+        self._cheese = None
+        self._tomato = None
+        self._lettuce = None
+        self._meat = None

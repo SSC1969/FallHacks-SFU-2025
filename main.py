a5def3e4e89fee3845bdbdbd7fbb5e3e6c3bf901
duhdiamond <duhdiamond76@gmail.com>
2025-09-27 17:39:25 -0700

    Trying to get sprites to work

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ import enum
 
 pygame.init()
 
-import pygame
-
 from src.game import Game
 
 game = Game()
 game.run()
 
+
+character = player(0, 0)
+# starts at 0, 0
+all_sprites = pygame.sprite.Group(character)
 
 running = True
 winCondition = False
@@ -28,6 +30,13 @@ while running:
             mouse_x, mouse_y = pygame.mouse.get_pos()
             snapped = pixel_to_grid(mouse_x, mouse_y)
             player_x, player_y = snapped
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP: character.moveUp()
+            elif event.key == pygame.K_DOWN: character.moveDown()
+            elif event.key == pygame.K_LEFT: character.moveLeft()
+            elif event.key == pygame.K_RIGHT: character.moveRight()
+    
+    all_sprites.draw(SCREEN)
 
     pygame.display.flip()
     dt = CLOCK.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,13 +1,36 @@
 from config import player_x, player_y, PLAYER_COLOUR
 from draw_logic import LEVEL1
+import pygame
 
-class player:
+class player(pygame.sprite.Sprite):
     player_x = 0
     player_y = 0
 
+    width = 50
+    height = 50
+    SURFACE_COLOUR = (167, 255, 100)
+    COLOUR = (255, 100, 98)
+
     def __init__(self, player_x, player_y):
+        super().__init__()
+
+        sheet = pygame.image.load("assets/player.png").convert_alpha()
+        
+        # Define area for frame
+
+        frame_rect = pygame.Rect(0, 0, self.width, self.height)
+        # e.g. top-left tile; move the rect to pick the sprite used in the spritesheet
+
+        # "Cut out" the frame
+        self.image = sheet.subsurface(frame_rect).copy()
+
+        # Position the sprite
+        self.rect = self.image.get_rect(topleft=(player_x, player_y))
+
         self.player_x = player_x
         self.player_y = player_y
+
+
 
     def moveUp(self):
         self.player_y += 1
@@ -16,10 +39,10 @@ class player:
         self.player_x -= 1
 
     def moveRight(self):
-        self.player.x += 1
+        self.player_x += 1
 
-    def moveLeft(self):
-        self.player.x -= 1
+    def moveDown(self):
+        self.player_y -= 1
 
     def collisionCheckWall(self):
         tile = LEVEL1[self.player_y][self.player_x]
